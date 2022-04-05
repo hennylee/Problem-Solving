864e4f50ce87d5a4a288cd7d475ae653be0c459e
hennylee <77392444+hennylee@users.noreply.github.com>
2022-04-05 23:26:40 +0000

    04_01 재풀이

--- 04_01.cpp
+++ 04_01.cpp
@@ -2,10 +2,14 @@
  * date : 2022-03-31
  * topic : 구현
  * feedback : 
+ *  1. 공간을 벗어나는 경우 고려 x 
+ *  2. 갯수를 알 수 없는 char 배열 입력은 string으로 받기
  * time :  15 min
  ************************/
 
 #include <iostream>
+//#include <istream>
+#include <string>
 using namespace std;
 
 int main()
@@ -13,11 +17,14 @@ int main()
   // 입력 받기
   int cnt;
   cin >> cnt;
+  cin.ignore(); // 버퍼 비우기
 
-  char path[cnt];
-  for(int x=0;x<cnt;x++){
-    cin >> path[x];
-  }
+  /*
+  char path[100];
+  cin.getline(path, 100);
+  */
+  string path;
+  getline(cin, path);
 
   // 방향 설정
   char direct[4] = {'L', 'R', 'U', 'D'};
@@ -29,13 +36,15 @@ int main()
   int yy = 1;
 
   // 경로 이동
-  for(int x=0;x<cnt;x++){
+  for(int x=0;x<path.length();x++){
     for(int y=0;y<4;y++){
       if(path[x] == direct[y]){
         xx += dx[y];
-        yy += dy[y];
+        yy += dy[y];  
       }
     }
+    // 공간을 벗어나는 경우
+    if(xx < 1 || yy < 1 || xx > cnt || yy > cnt) continue;
   }
 
   // 좌표 출력
